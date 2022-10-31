29345577ad2cba63a7e29c9b84950554832f1efb
ricardorojass <reddrum7@gmail.com>
2022-10-31 13:24:39 -0500

    create address module

---
+++ src/address/address.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAddressDto } from './dto/create-address.dto';
+import { UpdateAddressDto } from './dto/update-address.dto';
+
+@Injectable()
+export class AddressService {
+  create(createAddressDto: CreateAddressDto) {
+    console.log('====================================');
+    console.log('createAddressDto', createAddressDto);
+    console.log('====================================');
+    return 'This action adds a new address';
+  }
+
+  findAll() {
+    return `This action returns all address`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} address`;
+  }
+
+  update(id: number, updateAddressDto: UpdateAddressDto) {
+    return `This action updates a #${id} address`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} address`;
+  }
+}
---
+++ src/address/dto/create-address.dto.ts
@@ -0,0 +1,6 @@
+export class CreateAddressDto {
+  firstname: string;
+  surname: string
+  address: string;
+  phone: string;
+}
